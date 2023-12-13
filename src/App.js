98d4407ea2fd6ac4e04434f8eea988162bbd2e1b
IIHT Developers <93914497+IIHTDevelopers@users.noreply.github.com>
2023-12-13 11:16:49 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Calculator from './components/Calculator';
+
+function App() {
+  return (
+    <div className="App">
+      <h2>My Calculator App</h2>
+      <main>
+        <Calculator />
+      </main>
+    </div>
+  );
+}
+
+export default App;
